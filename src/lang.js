fafab623aefcdb1f755efa227ee048e79efd6f34
Максим Синявський <killtan@Air-KilltaN.fritz.box>
2023-03-08 01:50:32 +0100

    ukr

---
+++ src/lang.js
@@ -0,0 +1,30 @@
+const langArr = {
+    "unit": {
+        "ua": "БФ 'Нових Змін'",
+        "en": "CF of New Changes",
+    },
+    "odessaFond": {
+        "ua": "Одеса фонд і ГО",
+        "en": "Odessa fond",
+    },
+    "odessaRegion": {
+        "ua": "Суворовский р-н",
+        "en": "Suvorskyi r-n",
+    },
+    "odessaSteet": {
+        "ua": "Махачкалинская, 1",
+        "en": "Makhchkalynska st. 1",
+    },
+    "odessaHost": {
+        "ua": "Заїка Максим Петрович",
+        "en": "Zaika Maksym Petrovych",
+    },
+    "victoryClockText": {
+        "ua": "Ми єдині вже:",
+        "en": "Ukraine together about:",
+    },
+    "change": {
+        "ua": "Вибір мови:",
+        "en": "Language change:",
+    },
+}
---
+++ src/script.js
@@ -0,0 +1,61 @@
+document.getElementById('odessa').onclick = function() {
+    let openPopUp = document.getElementById('odessa');
+    let closePopUp = document.getElementById('popUpClose');
+    let popUp = document.getElementById('popUp')
+
+    openPopUp.addEventListener('click', function(e) {
+        popUp.classList.add('active');
+    })
+
+    closePopUp.addEventListener('click', () => {
+        popUp.classList.remove('active')
+    })
+}
+
+document.getElementById('navbarLogo').onclick = function() {
+    document.location.assign('./index.html')
+}
+
+window.setInterval(function() {
+    let nowDate = new Date();
+    let warDate = new Date(2022, 1, 24, 4);
+    warDate = warDate.getTime() + 2 * (3600 * 1000);
+    let nowWarDate = nowDate - warDate;
+    let hours = Math.floor(nowWarDate / (1000 * 60 * 60));
+    let minutes = nowDate.getMinutes();
+    let seconds = nowDate.getSeconds();
+    if(seconds < 10){
+        seconds = "0" + seconds;
+    }
+    if(minutes < 10){
+        minutes = "0" + minutes;
+    }
+    
+    let clock = hours + ':' + minutes + ':' + seconds;
+    document.getElementById("clock").innerHTML = clock;
+})
+
+const select = document.querySelector('select');
+const allLang = ['ua', 'en'];
+
+select.addEventListener('change', changeURLLanguage);
+
+function changeURLLanguage() {
+    let lang = select.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+function changeLanguage() {
+    let hash = window.location.hash;
+    hash = hash.substr(1);
+    if(!allLang.includes(hash)) {
+        location.href = window.location.pathname + "#ua";
+        location.reload();
+    }
+    select.value = hash;
+    for (let key in langArr) {
+        document.getElementById(key).innerHTML = langArr[key][hash];
+    }
+}
+changeLanguage();

7bbcfee3876941dcd47ef6868692c6ca23d7d4c0
Максим Синявський <killtan@Air-KilltaN.fritz.box>
2023-05-21 01:40:17 +0200

    odesa-fond

--- src/lang.about.js
+++ src/lang.about.js
@@ -7,4 +7,64 @@ const langArr = {
         "ua": "Вибір мови:",
         "en": "Language change:",
     },
+    "home": {
+        "ua": "На головну",
+        "en": "Home page",
+    },
+    "partners": {
+        "ua": "Наші партнери",
+        "en": "Our partners",
+    },
+    "help": {
+        "ua": "Бажаю допомогти",
+        "en": "I want to help",
+    },
+    "reportTitle": {
+        "ua": "Наш звіт",
+        "en": "Our report",
+    },
+    "patrulText": {
+        "ua": "Команда БФ 'НОВИХ ЗМІН' з початком повномасштабного вторгнення стала на оборону м.Одеси. Вдень та вночі патрулювали місто задля безпеки громадян.",
+        "en": "With the beginning of a full-scale invasion, the team of the CF 'NOVYH ZMIN' began to defend the city of Odessa. They patrolled the city day and night for the safety of citizens.",
+    },
+    "carText": {
+        "ua": "Благодійний фонд придбав та передав військовим 2 автомобілі",
+        "en": "The charity foundation purchased and handed over 2 cars to the military",
+    },
+    "helpToArmyText": {
+        "ua": "Силами небайдужих людей було зібрано та передано для наших бійців термобілизну, ковдри, матраци, подушки, мікрохвильові печі та інше приладдя для поліпшення перебування на передовій.",
+        "en": "Thanks to the efforts of caring people, thermal clothing, blankets, mattresses, pillows, microwave ovens and other supplies were collected and handed over to our soldiers to improve their stay on the front lines.",
+    },
+    "armorText": {
+        "ua": "Пошили та зкомплектували 62 бронежелети 4 класу для наших захисників",
+        "en": "We sewed and assembled 62 class 4 body armor for our defenders",
+    },
+    "signalText": {
+        "ua": "Для поліпшення сигналу, та можливості прослуховування ворожих переговорів нами було придбано та передано посилювач радіосигналу Motorola ",
+        "en": "In order to improve the signal and the possibility of listening to enemy negotiations, we purchased and transferred a Motorola radio signal amplifier",
+    },
+    "interhimHalpText": {
+        "ua": "Завдяки нашому партнеру компанії 'ІНТЕРХІМ' ми змогли забезпечити наших захисників високоякісними препаратами для підтримки імунітету та боротьби з простудних та вірусними захворюваннями. Наші співвітчизники за кордоном не сиділи склавши руки, звичайні небайдужі люди та  Європейські партнери допомогли  перев'язочними матеріалами военним госпіталям Миколаївської та Одеської областей.",
+        "en": "Thanks to our partner 'INTERCHEM' company, we were able to provide our defenders with high-quality drugs to support immunity and fight against colds and viral diseases. Our compatriots abroad did not sit idly by, ordinary caring people and European partners helped with dressing materials to the military hospitals of the Mykolaiv and Odesa regions.",
+    },
+    "fondOwnerTitle": {
+        "ua": "Наш засновник",
+        "en": "Our founder",
+    },
+    "fondOwnerName": {
+        "ua": "Мальцев Олександр Павлович",
+        "en": "Oleksandr Pavlovich Maltsev",
+    },
+    "ownerInformationMain": {
+        "ua": "Батько- Курочкін Павло Дмитрович, військовий в званні прапорщика ВПС, в даний час на пенсії. Мати- Курочкіна Валентина Іванівна  вчитель російської мови та літератури, в даний час на пенсії. Олександр Павлович закінчив дошкільний навчальний заклад №4 ЯСЛА-САДОК 'СОНЕЧКО' в м. Арциз. B 1991 році в віці 7 років Олександр вступив в Арцизький заклад загальної середньої освіти № 4. В шкільні роки займався вуличними танцями «брей-данс»,  Таеквон-До, боксом,  грав у баскетбол. Приймав участь в шкільній самодіяльності, грав у команді КВК. B 2001 році вступив до Харківського  національного університету Повітряних Сил імені Івана Кожедуба...",
+        "en": "Father - Pavlo Dmytrovych Kurochkin, a military officer with the rank of ensign in the Air Force, currently retired. Mother - Kurochkina Valentyna Ivanivna is a teacher of Russian language and literature, currently retired. Oleksandr Pavlovich graduated from preschool educational institution No. 4 'SONECHKO' NURSERY-KINDERGARTEN in the city of Artsyz. In 1991, at the age of 7, Oleksandr entered the Artsysk general secondary education institution No. 4. During his school years, he was involved in street dancing, bray dancing, Taekwon-Do, boxing, and played basketball. Took part in school amateur activities, played in the KVK team. B In 2001, he entered the Kharkiv National University of the Air Force named after Ivan Kozhedub...",
+    },
+    "moreText": {
+        "ua": "Батько- Курочкін Павло Дмитрович, військовий в званні прапорщика ВПС, в даний час на пенсії. Мати- Курочкіна Валентина Іванівна  вчитель російської мови та літератури, в даний час на пенсії. Олександр Павлович закінчив дошкільний навчальний заклад №4 ЯСЛА-САДОК 'СОНЕЧКО' в м. Арциз. В 1991 році в віці 7 років Олександр вступив в Арцизький заклад загальної середньої освіти № 4. В шкільні роки займався вуличними танцями «брей-данс»,  Таеквон-До, боксом,  грав у баскетбол. Приймав участь в шкільній самодіяльності, грав у команді КВК. В 2001 році вступив до Харківського  національного університету Повітряних Сил імені Івана Кожедуба. В 2006 році здобув вищу освіту за спеціальністю інженера по експлуатаціi та проектуванню лiтаючих засобів. Під час навчання в університеті двічі був нагороджений подякою від Міністра оборони України. Відмінник політичної та бойової підготовки. Переможець змагань зі стрілкової підготовки збройних сил України в Харківській області. З 2006 року працював на ЗАТ «Харкiвiнструментпiдшипник» м.Харків, починаючи з рядового слюсара-римонтника, домігся підвищення до Менеджера зі сбуту. Показавши свої професійні навички та цілеспрямованість направлену на результат був запрошений на таку ж посаду на Завод iменi Фрунзе м.Харків, вже на прикінці 2006 році отримав підвищення до Економiста вiддiлу продажу, з 2007 року працював ведучім економістом. В 2008 році звільнився з посади ведучого економіста с Завода iменi Фрунзе, та почав власну справу відкривши СТО (станція технічного обслуговування) «АвтопроТранс» з 2008 року до 2023 року ФОП Мальцев О.П. З 2012-2013 працював заступником директора філії  ПП АТП «Транс-ТехСервiс Фаворит» м.Київ. в 2013 році був переведений до філії в м.Харків Директором  ПП АТП «Транс-ТехСервiс Фаворит» м.Харкiв. В 2014 Олександр Павлович заснував «Всеукраїнське Таксi 306» в найбільших містах України: м.Харків, м.Київ, м. Одеса, м.Дніпропетровськ, а також у своєму рідному місті Арциз. До 2018 року працював директором власного таксі. В 2019 році заснував ТОВ «ДіНас ГРУПП» В 2020 році заснував  «БЛАГОДІЙНИЙ ФОНД Олександра Мальцева «НОВИХ ЗМІН» В 2022 р. Вступив до МГУ (Міжнародний гуманітарний університет) з метою отримання 2-ї вищої освіти на факультет Міжнародних економічних відносин. В 2023 році став лідером Народного руху 'Нових змін', який націлений на поліпшення життя українського народу та боротьбу з корумпованою владою. Знання мов: Російська – вільно, Українська – вільно,  Англiйська-зi словником. Член групи активiстiв з написання та впровадження закону «Про лiцензуванню та оподаткуванню сфери таксi». Волонтер, активiст з 2014 (Розселення біженців в Харківській області, збір і відправлення продуктів харчування в зону АТО). Постійна допомога хворим дітям на території України. Постійна допомога ветеранам, пенсіонерам та інвалідам України. Член правлiння ОСББ 'Альянс 2007' з 2014-2019 р.р.Член правлiння ГБК (гаражно-будівельний кооператив)  «Сiрiус Плюс»  з 2019 р. Голова екзаменаційної комісії в ПТУ №76 з 2011-2016 р.р. Активний член «Всеукраїнської  професійної асоціації підприємців» з 2020 року. З 2020-2023 Директор Благодійної організації 'Благодійний фонд' 'НОВИХ ЗМІН'. На початку повномасштабного вторгнення очолював добровольчиц батальйон та з командою патрулював Суворовський район м.Одеси. Під командуванням Олександра будувалися захисні та протитанкові споруди, також батальйон виявляв колоборантів та диверсійні групи. Під час патрулювання перешкоджав розкрадання майна у місцевого населення. Боровся з розкраданням гуманітарної допомоги. Березень-квітень 2022р. Частина добровольців з батальону під командуванням Олександра захищали м.Миколаїв від Російських окупантів. До теперішнього часу допомагає ЗСУ та мирному населенню постраждалому від окупанта. Неодружений, маю двох доньок",
+        "en": "Father - Pavlo Dmytrovych Kurochkin, a military officer with the rank of ensign in the Air Force, currently retired. Mother - Kurochkina Valentyna Ivanivna is a teacher of Russian language and literature, currently retired. Oleksandr Pavlovich graduated from preschool educational institution No. 4 'SONECHKO' NURSERY-KINDERGARTEN in the city of Artsyz. In 1991, at the age of 7, Oleksandr entered the Artsysk general secondary education institution No. 4. During his school years, he was involved in street dancing, Taekwon-Do, boxing, and played basketball. Took part in school amateur activities, played in the KVK team. In 2001, he entered the Kharkiv National Air Force University named after Ivan Kozhedub. In 2006, he obtained a higher education in the specialty of engineer in the operation and design of flying vehicles. During his studies at the university, he was twice awarded with thanks from the Minister of Defense of Ukraine. Excellent political and combat training. The winner of the marksmanship training competition of the Armed Forces of Ukraine in the Kharkiv region. Since 2006, he worked for Kharkiv Instrument Bearing CJSC, starting as an ordinary locksmith-repairman, and was promoted to Sales Manager. Having shown his professional skills and determination aimed at results, he was invited to the same position at the Frunze Plant in Kharkiv, at the end of 2006 he was promoted to the position of Economist of the sales department, since 2007 he worked as a leading economist. In 2008, he resigned from the position of leading economist at the Frunze Plant, and started his own business by opening the 'AutoproTrans' service station (maintenance station) from 2008 to 2023. FOP Maltsev O.P. From 2012 to 2013, he worked as the deputy director of the Kyiv branch of Trans-TechService Favorite ATP. in 2013, he was transferred to the branch in Kharkiv by the Director of Trans-TechService Favorite ATP, Kharkiv. In 2014, Oleksandr Pavlovich founded 'All-Ukrainian Taxi 306' in the largest cities of Ukraine: Kharkiv, Kyiv, Odesa, Dnipropetrovsk, as well as in his hometown Artsyz. Until 2018, he worked as a director of his own taxi. In 2019, he founded DiNas GROUP LLC. In 2020, he founded the Oleksandr Maltsev CHARITY FUND 'NEW CHANGES' In 2022, he entered Moscow State University (International Humanitarian University) with the aim of obtaining a 2nd higher education at the Faculty of International Economic Relations. In 2023, he became the leader of the 'New Changes' People's Movement, which aims to improve the lives of the Ukrainian people and fight against the corrupt government. Knowledge of languages: fluent Russian, fluent Ukrainian, English with a dictionary. Member of the group of activists on the drafting and implementation of the law 'On Licensing and Taxation of the Taxi Sector'. Volunteer, activist since 2014 (Resettlement of refugees in the Kharkiv region, collection and delivery of food products to the ATO zone). Permanent assistance to sick children on the territory of Ukraine. Permanent assistance to veterans, pensioners and disabled people of Ukraine. Member of the board of the condominium 'Alliance 2007' from 2014-2019. Member of the board of the GBK (garage-building cooperative) 'Sirius Plus' since 2019. Chairman of the examination commission at PTU #76 from 2011-2016. Active member of the 'All-Ukrainian Professional Association of Entrepreneurs' since 2020. From 2020 to 2023, Director of the Charitable Organization 'Charitable Fund' 'NEW CHANGES'. At the beginning of the full-scale invasion, a volunteer battalion led and patrolled the Suvorovsky district of Odessa with a team. Under Alexander's command, defensive and anti-tank structures were built, and the battalion identified collaborators and sabotage groups. During the patrol, he prevented the theft of property from the local population. Struggled with embezzlement of humanitarian aid. March-April 2022 Part of the volunteers from the battalion under the command of Oleksandr defended the city of Mykolaiv from the Russian invaders. Until now, it helps the Armed Forces and the civilian population affected by the occupier. I am single, I have two daughters.",
+    },
+    "more": {
+        "ua": "Детальнiше",
+        "en": "Read more",
+    },
 }
--- src/script.js
+++ src/script.js
@@ -30,6 +30,13 @@ partners.onmouseleave = handleMouseLeave;
 partnersBox.onmousemove = handleMouseOver;
 partnersBox.onmouseleave = handleMouseLeave;
 
+document.getElementById('formCansel').onclick = function() {
+    document.getElementById('formDonate').style.display = "none";
+}
+
+document.getElementById('help').onclick = function() {
+    document.getElementById('formDonate').style.display = "block";
+}
 
 document.getElementById('interhimPartner').onclick = function() {
     window.open('https://interchem.ua/', '_blank');
